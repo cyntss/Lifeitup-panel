f18a75dedafb3d9d298fe3d5c9a5f504bbbe3ee7
Cynthia Sanchez <cynt005@gmail.com>
2014-10-29 00:29:03 +0100

    layout responsive wih footer at bottom

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -2,20 +2,42 @@
 
 $(window).on("load", function() {
   
-  layout_resizer ()
+  layout_resizer ();
 
 });
 
-$(window).on("resize", layout_resizer ());
+$(window).on("resize", function() {
+
+  layout_resizer();
+
+});
 
 function layout_resizer () {;
-  var screenHeight = $(document).height();
-  var headerHeight = $("header").height();
-  var footerHeight = $("footer").height();
+  var screenHeight   = $(window).height();
+  var headerHeight   = $("header").outerHeight();
+  var footerHeight   = $("footer").outerHeight();
+  var asideHeight    = $("aside ul").outerHeight();
+  var sectionHeight  = $("section").outerHeight();
 
-  var containerHeight = screenHeight - headerHeight - footerHeight;
+  if ( ( headerHeight + footerHeight + asideHeight ) > screenHeight && asideHeight > sectionHeight ) {
   
-  $(".container-fluid").css({
-    height : containerHeight + "px"
-  });
+    $(".container-fluid").css({
+      height : asideHeight + "px"
+    });
+
+  } else if ( ( headerHeight + footerHeight + sectionHeight ) > screenHeight && asideHeight < sectionHeight) {
+
+    $(".container-fluid").css({
+      height : sectionHeight + "px"
+    });
+
+  } else {
+
+    $(".container-fluid").css({
+      height : screenHeight - headerHeight - footerHeight + "px"
+    });
+
+  }
+
 }
+
